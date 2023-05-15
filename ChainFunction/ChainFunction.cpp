8776bd0e14d7c0b529a8ad396a6ca3e4e4db6b5d
salss33 <hallosalsa03@gmail.com>
2023-05-15 22:02:22 +0700

    bukunya

--- ChainFunction/ChainFunction.cpp
+++ ChainFunction/ChainFunction.cpp
@@ -6,11 +6,13 @@ class buku
 
 public:
 	buku setJudul(string judul)
-		{
-			this->judul = judul;
-	return *this; // chain function
-		}
-		string getJudul()
-		{
-			return this->judul;
-        }
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+
+} bukunya;
