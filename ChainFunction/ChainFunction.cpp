793be2905a9b3c176b0a8affb40368a283437dd2
salss33 <hallosalsa03@gmail.com>
2023-05-15 22:01:50 +0700

    buku

--- ChainFunction/ChainFunction.cpp
+++ ChainFunction/ChainFunction.cpp
@@ -5,11 +5,12 @@ class buku
 	string judul;
 
 public:
-	buku setJudul(string judul
+	buku setJudul(string judul)
 		{
 			this->judul = judul;
-	return*this; //chain function
+	return *this; // chain function
 		}
-			
-
-};
+		string getJudul()
+		{
+			return this->judul;
+        }
